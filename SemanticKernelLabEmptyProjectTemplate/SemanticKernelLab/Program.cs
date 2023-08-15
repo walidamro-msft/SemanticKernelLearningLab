2c3e5c06af130112bb16e79ea698fe17fde25a5e
Walid Amro <60905923+walidamro-msft@users.noreply.github.com>
2023-08-15 18:21:17 -0500

    Add files via upload

---
+++ SemanticKernelLabEmptyProjectTemplate/SemanticKernelLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SemanticKernelLab
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            string azureOpenAiServiceEndpoint = configuration.GetSection("AzureOpenAiServiceEndpoint").Value;
+            string azureOpenAiServiceKey = configuration.GetSection("AzureOpenAiServiceKey").Value;
+            string azureOpenAiServiceDeploymentName = configuration.GetSection("AzureOpenAiServiceDeploymentName").Value;
+            string bingSearchServiceKey = configuration.GetSection("BingSearchServiceKey").Value;
+        }
+    }
+}
